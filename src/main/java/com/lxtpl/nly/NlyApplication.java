4ab676ba1093af192f08669d961c94169d0a5abb
liuxutao <3387444325@qq.com>
2023-06-04 19:01:27 +0800

    工程初始化

---
+++ src/main/java/com/lxtpl/nly/NlyApplication.java
@@ -0,0 +1,13 @@
+package com.lxtpl.nly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lxtpl/nly/NlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxtpl.nly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
